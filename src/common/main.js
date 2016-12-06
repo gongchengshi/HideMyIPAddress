19b88f740acdab7ad0cbf84d2d130639e677fa17
Jeremy McLain <gongchengshi@gmail.com>
2016-12-06 11:55:13 -0800

    Initial commit

---
+++ src/common/main.js
@@ -0,0 +1,49 @@
+﻿//var {Cc, Ci} = require("chrome");
+var httpRequestObserver =
+{
+   observe: function(subject, topic, data)
+   {
+      if (topic == "http-on-modify-request") {
+         var httpChannel = subject.QueryInterface(Ci.nsIHttpChannel);
+         httpChannel.setRequestHeader("Client-ip", "0.0.0.0", false);
+         httpChannel.setRequestHeader("X-forwarded-for", "0.0.0.0", false);
+      }
+   },
+
+   get observerService() {
+      return Cc["@mozilla.org/observer-service;1"]
+         .getService(Ci.nsIObserverService);
+   },
+
+   register: function()
+   {
+      this.observerService.addObserver(this, "http-on-modify-request", false);
+   },
+
+   unregister: function()
+   {
+      this.observerService.removeObserver(this, "http-on-modify-request");
+   }
+};
+
+function MyExtension() {
+	var self = this;
+
+   browserName = kango.browser.getName();
+   if(browserName == 'chrome') {
+      chrome.webRequest.onBeforeSendHeaders.addListener(
+         function(details) {
+            details.requestHeaders.push({name: 'Client-ip', value: '0.0.0.0'});
+            details.requestHeaders.push({name: 'X-forwarded-for', value: '0.0.0.0'});
+            return {requestHeaders: details.requestHeaders};
+         },
+         {urls: []},
+         ['requestHeaders', 'blocking']
+      );
+   } else if(browserName == 'firefox') {
+      httpRequestObserver.register();
+      console.log("registered httpRequestObserver");
+   }
+}
+
+var extension = new MyExtension();
